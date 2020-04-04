ea413364bca20d57b7f3881702ae0239c0c0008b
nileshdabhivedaha <nilesh.d@vedaha.com>
2020-04-04 11:13:23 +0530

    add project

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use Auth;
+use Exception;
+use App\User;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+	public function redirectToFacebook() {
+        return Socialite::driver('facebook')->redirect();
+    }
+    public function handleFacebookCallback() {
+        try {
+            $user = Socialite::driver('facebook')->user();
+            $finduser = User::where('facebook_id', $user->id)->first();
+            if ($finduser) {
+                Auth::login($finduser);
+                return redirect('/home');
+            } else {
+                $newUser = User::create(['name' => $user->name, 'email' => $user->email, 'facebook_id' => $user->id]);
+                Auth::login($newUser);
+                return redirect()->back();
+            }
+        }
+        catch(Exception $e) {
+            return redirect('auth/facebook');
+        }
+    }
+}
